28336351a634872f8c3fe0e4029ebca4b14d8491
hassaan Hameed <hassaanhameed3135@gmail.com>
2022-12-12 16:48:46 +0500

    udated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import WellCome from "./Component/WelComePage/WellCome";
 import MainFeedNews from "./Component/NewFeed/MainNewFeed/MainFeedBack";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import SignUp from "./Component/SignPage/SignUp/SignUp";
+
 import SignIn from "./Component/SignPage/SignIn/SignIn";
 import Verification from "./Component/SignPage/Verification/Verification";
 import MangNav from "./Component/Navbar/MangNav";
@@ -31,7 +32,7 @@ import UBlock from "./Component/DeskTopSetting/UserBlock/UBlock";
 import DelAccount from "./Component/DeskTopSetting/DelAccout/DelAccount";
 // import Data from "./ReuseableCode/Date/Data";
 import NewBox from "./Component/NewFeed/MiddUse/FedBox/NewBox";
-import AboutPost from './Component/AdminProfile/RightSide/Info&About/About/AboutPost/AboutPop'
+import AboutPost from "./Component/AdminProfile/RightSide/Info&About/About/AboutPost/AboutPop";
 // import EduAdd from './Component/AdminProfile/RightSide/Education/Popup/EduPUT'
 import SkillAdd from "./Component/AdminProfile/RightSide/Skill/SkillPopup/SkillAdd";
 import SkillPut from "./Component/AdminProfile/RightSide/Skill/SkillPopup/SkillPut";
@@ -40,7 +41,7 @@ import AdminProfile from "./Component/AdminProfile/MainFile/AdminProfile";
 import ExpPUT from "./Component/AdminProfile/RightSide/Experience/ExperiencePop/ExpPUT";
 import PostBody from "./Component/AdminProfile/RightSide/PostPage/PostBody";
 import PostGallary from "./Component/AdminProfile/RightSide/PostPage/PostGallary/PostGallary";
-import ImgUpload from './Component/NewFeed/MiddUse/header/fileupload/ImgUpload'
+import ImgUpload from "./Component/NewFeed/MiddUse/header/fileupload/ImgUpload";
 import Rana from "./Component/NewFeed/MiddUse/header/fileupload/Rana";
 import FimnalIMG from "./Component/NewFeed/MiddUse/header/fileupload/FimnalIMG";
 import FinalIMG from "./Component/NewFeed/MiddUse/header/fileupload/FinalIMG";
@@ -48,10 +49,10 @@ import PostGallay1 from "./Component/AdminProfile/RightSide/PostPage/PostGallary
 import SettingExp from "./Component/DeskTopSetting/Skill&Exp&Pro/Experince/SettingExp";
 import SettingPro from "./Component/DeskTopSetting/Skill&Exp&Pro/Project/SettingPro";
 import TwoFect3 from "./Component/DeskTopSetting/TwoFectorVerification/TwoFector3/TwoFect3";
-import Forget1 from './Component/DeskTopSetting/ForgetPassword/Forget1/Forget1'
-import Forget2 from './Component/DeskTopSetting/ForgetPassword/Forget2/Forget2'
+import Forget1 from "./Component/DeskTopSetting/ForgetPassword/Forget1/Forget1";
+import Forget2 from "./Component/DeskTopSetting/ForgetPassword/Forget2/Forget2";
 import Coming from "./ReuseableCode/ComingSoon/Coming";
-import { gapi } from 'gapi-script'
+import { gapi } from "gapi-script";
 import GoogleLogins from "./Component/SignPage/Google/GoogleLogin";
 // import UserProfile from "./ReuseableCode/UserProfile/MainVisitUser/UserVisitProfile";
 import UserVisitProfile from "./ReuseableCode/UserVisitProfile/MainVisitUser/UserVisitProfile";
@@ -60,63 +61,59 @@ import UCompanyPro from "./Component/UserCompanyProfile/UserCompantProfile/UComp
 import FaceBoook from "./Component/AdvanceLogin/FaceBoook";
 import GoogleLogin from "./Component/AdvanceLogin/GoogleLogin";
 import ExpTest from "./Component/AdminProfile/RightSide/Experience/ExperiencePop/ExpTest";
-import MUI from './ReuseableCode/MUI/MUI'
-import jwtDecode from 'jwt-decode';
+import MUI from "./ReuseableCode/MUI/MUI";
+import jwtDecode from "jwt-decode";
 let clientId =
   "233617417166-1v5f6fov7ijsf425ekr0r27kb2m3ab7p.apps.googleusercontent.com";
 
 function App({ ExpVisitApp }) {
   const [SkillData, setSkillData] = useState([]);
   const [NavM, setNavM] = useState(false);
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState({});
 
   console.log(user);
   // const [ExpvisitTwo, setExpvisitTwo] = useState();
   // console.log('visit two app', ExpvisitTwo);
-  
-  const navigate = useNavigate()
-  
-  
+
+  const navigate = useNavigate();
+
   const handleCallbackResponse = (response) => {
-    console.log('Encoded JWT ID Token: ', response.credential);
-    var userObject = jwtDecode(response.credential)
-    setUser(userObject)
-    document.getElementById('signInDiv').hidden = true;
-    setNavM(true)
-  }
+    console.log("Encoded JWT ID Token: ", response.credential);
+    var userObject = jwtDecode(response.credential);
+    setUser(userObject);
+    document.getElementById("signInDiv").hidden = true;
+    setNavM(true);
+  };
 
   const handleSignout = (event) => {
-    setUser({})
-    document.getElementById('signInDiv').hidden = false;
-    setNavM(false)
-  }
+    setUser({});
+    document.getElementById("signInDiv").hidden = false;
+    setNavM(false);
+  };
 
   useEffect(() => {
     window.google.accounts.id.initialize({
       client_id: clientId,
-      callback: handleCallbackResponse
-    })
+      callback: handleCallbackResponse,
+    });
     window.google.accounts.id.renderButton(
-      document.getElementById('signInDiv'),
-      {theme:'outline', size:'large'}
-    )
-    window.google.accounts.id.prompt()
+      document.getElementById("signInDiv"),
+      { theme: "outline", size: "large" }
+    );
+    window.google.accounts.id.prompt();
     // google_global
   }, []);
 
   useEffect(() => {
-    
-  
     function start() {
       gapi.client.init({
         ClientId: clientId,
-        scope: ''
-      })
-    };
+        scope: "",
+      });
+    }
 
     // console.log('client:auth2', start);
-  }, [])
-
+  }, []);
 
   function UserData(UserSkillList) {
     setSkillData(UserSkillList);
@@ -131,13 +128,10 @@ function App({ ExpVisitApp }) {
   useEffect(() => {
     if (authUser) {
       if (authUser.status == true) {
-        setNavM(authUser.status)
+        setNavM(authUser.status);
       }
     }
-  }, [authUser])
-
-
-
+  }, [authUser]);
 
   //  if(authUser.status == true){
   //      setNavM(authUser.status)
@@ -145,118 +139,101 @@ function App({ ExpVisitApp }) {
 
   // let ExpvisitTwoa = ExpvisitTwo.length > 0 && <UserProfile ExpData={ExpvisitTwo} />
 
-
   return (
     <div>
-      {NavM ? <MangNav handleSignout={handleSignout} /> : ''}
+      {NavM ? <MangNav handleSignout={handleSignout} /> : ""}
       <Routes>
         {/* <Route path="/" element={<MangNav />} /> */}
         {/* <Route path="skillreuseable"  element={<SkillReuseable SkillData={UserData} />}/> */}
-        
-  
-{
-  NavM ? (<><Route path="/googlelogin" element={<GoogleLogin />} />
-  {/* <Route path="/userprofile" element={<UserProfile />} /> */}
-  <Route path="/userprofile" element={<UserVisitProfile />} />
-  {/* <Route path="/nav" element={<Nav />} /> */}
-  <Route path="/facebook" element={<FaceBoook />} />
-  {/* <Route path="/mainnav" element={<MangNav />} /> */}
 
+        {NavM ? (
+          <>
+            <Route path="/googlelogin" element={<GoogleLogin />} />
+            {/* <Route path="/userprofile" element={<UserProfile />} /> */}
+            <Route path="/userprofile" element={<UserVisitProfile />} />
+            {/* <Route path="/nav" element={<Nav />} /> */}
+            <Route path="/facebook" element={<FaceBoook />} />
+            {/* <Route path="/mainnav" element={<MangNav />} /> */}
 
-  {/* small Setting routing */}
-  <Route path="/general" element={<GSetting />} />
-  <Route path="/notification" element={<Notify />} />
-  <Route path="/profile" element={<Profile />} />
-  {/* <Route path="/verification" element={<UProVerify />} /> */}
-  <Route path="/privacy" element={<Privacy />} />
-  <Route path="/password" element={<Password />} />
-  <Route path="/sessions" element={<ManagSession />} />
+            {/* small Setting routing */}
+            <Route path="/general" element={<GSetting />} />
+            <Route path="/notification" element={<Notify />} />
+            <Route path="/profile" element={<Profile />} />
+            {/* <Route path="/verification" element={<UProVerify />} /> */}
+            <Route path="/privacy" element={<Privacy />} />
+            <Route path="/password" element={<Password />} />
+            <Route path="/sessions" element={<ManagSession />} />
 
-  <Route path="/skillsmall" element={<Skill2 />} />
-  <Route path='/verify/:id' element={<SkillSmall />} />
+            <Route path="/skillsmall" element={<Skill2 />} />
+            <Route path="/verify/:id" element={<SkillSmall />} />
 
-  <Route path="/Authentication" element={<TwoFect1 />} />
-  <Route path="/Authentications/:id" element={<TwoFect2 />} />
-  <Route path="/userblocks" element={<UBlock />} />
-  <Route path="/deleteaccount" element={<DelAccount />} />
+            <Route path="/Authentication" element={<TwoFect1 />} />
+            <Route path="/Authentications/:id" element={<TwoFect2 />} />
+            <Route path="/userblocks" element={<UBlock />} />
+            <Route path="/deleteaccount" element={<DelAccount />} />
 
+            {/* AdminProfile */}
 
+            <Route path="/myprofile" element={<AdminProfile />} />
+            {/* <Route path="/exptest" element={<ExpTest />} /> */}
+            <Route path="/mui" element={<MUI />} />
 
+            {/* news feed */}
 
+            <Route path="/newsfeed" element={<MainFeedNews />} />
+            <Route path="/postbody" element={<PostBody />} />
 
-
-  {/* AdminProfile */}
-
-  <Route path="/myprofile" element={<AdminProfile />} />
-  {/* <Route path="/exptest" element={<ExpTest />} /> */}
-  <Route path="/mui" element={<MUI />} />
-
-  {/* news feed */}
-  
-  <Route path="/newsfeed" element={<MainFeedNews />} />
-  <Route path="/postbody" element={<PostBody />} />
-
-    {/* sign in route */}
-  {/* <Route path="/signup" element={<SignUp />} />
+            {/* sign in route */}
+            {/* <Route path="/signup" element={<SignUp />} />
   <Route path="/signin" element={<SignIn setIsAuth={setIsAuth} />} />
   <Route path='/verification' element={<Verification />} />
   <Route path='/forget' element={<Forget1 />} />
   <Route path='/user_reset_password/:id/:id/' element={<Forget2 />} /> */}
-  <Route path='/imgupload' element={<ImgUpload />} />
-  <Route path='/rana' element={<Rana />} />
-  <Route path='/imgs' element={<FimnalIMG />} />
-  <Route path='/imgf' element={<FinalIMG />} />
-  <Route path='/post1' element={<PostGallay1 />} />
-  <Route path='/comingsoon' element={<Coming />} />
+            <Route path="/imgupload" element={<ImgUpload />} />
+            <Route path="/rana" element={<Rana />} />
+            <Route path="/imgs" element={<FimnalIMG />} />
+            <Route path="/imgf" element={<FinalIMG />} />
+            <Route path="/post1" element={<PostGallay1 />} />
+            <Route path="/comingsoon" element={<Coming />} />
 
+            {/* User Company routes */}
 
+            <Route path="/company" element={<UCompanyPro />} />
 
+            {/* People Route */}
 
+            <Route path="/people" element={<MainPeoplePage />} />
 
- {/* User Company routes */}
- 
-  <Route path='/company' element={<UCompanyPro />} />
+            {/* Routing */}
 
-  {/* People Route */}
+            <Route path="/settings/" element={<DeskTopProfile />} id="helojnb">
+              <Route path="general" element={<GSetting />} />
+              <Route path="notification" element={<Notify />} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="privacy" element={<Privacy />} />
+              <Route path="verification" element={<UProVerify />} />
+              <Route path="password" element={<Password />} />
+              <Route path="sessions" element={<ManagSession />} />
 
-  <Route path='/people' element={<MainPeoplePage />} />
- 
+              <Route path="skill" element={<Skill2 />} />
+              <Route path=":id" element={<Skill1 />} />
 
-  {/* Routing */}
+              <Route path="experience" element={<SettingExp />} />
+              <Route path="project" element={<SettingPro />} />
 
-  <Route path="/settings/" element={<DeskTopProfile />} id='helojnb'>
+              <Route path="authentication/" element={<TwoFect1 />} />
+              <Route path="authentications/:id" element={<TwoFect2 />} />
+              <Route path="entercode/:id" element={<TwoFect3 />} />
+              <Route path="userblock" element={<UBlock />} />
+              <Route path="delete-account" element={<DelAccount />} />
+            </Route>
 
-    <Route path="general" element={<GSetting />} />
-    <Route path="notification" element={<Notify />} />
-    <Route path="profile" element={<Profile />} />
-    <Route path="privacy" element={<Privacy />} />
-    <Route path="verification" element={<UProVerify />} />
-    <Route path="password" element={<Password />} />
-    <Route path="sessions" element={<ManagSession />} />
-
-    <Route path="skill" element={<Skill2 />} />
-    <Route path=':id' element={<Skill1 />} />
-
-    <Route path="experience" element={<SettingExp />} />
-    <Route path="project" element={<SettingPro />} />
-
-
-    <Route path="authentication/" element={<TwoFect1 />} />
-    <Route path="authentications/:id" element={<TwoFect2 />} />
-    <Route path="entercode/:id" element={<TwoFect3 />} />
-    <Route path="userblock" element={<UBlock />} />
-    <Route path="delete-account" element={<DelAccount />} />
-
-
-  </Route>
-
-
-  {/* Setting DeskTop  */}
-  {/* <Route path='/desktopseting/' element={<DeskTopSetting />}> */}
-  {/* <Route path='desktopsign' element={<MainDeskTopSign />} /> */}
-  {/* <Route path='visibility' element={<MainVisibilty />} /> */}
-  {/* </Route> */}
-  {/* <Route path='/communication' element={<MainCommunication />} />
+            {/* Setting DeskTop  */}
+            {/* <Route path='/desktopseting/' element={<DeskTopSetting />}> */}
+            {/* <Route path='desktopsign' element={<MainDeskTopSign />} /> */}
+            {/* <Route path='visibility' element={<MainVisibilty />} /> */}
+            {/* </Route> */}
+            {/* <Route path='/communication' element={<MainCommunication />} />
   <Route path='/advertising' element={<MainDeskAdvertising />} />
   <Route path='/addmail' element={<MainAddEmail />} />
   <Route path='/seureemailadd' element={<MainSecureEmail />} />
@@ -271,14 +248,17 @@ function App({ ExpVisitApp }) {
   <Route path='/profileview' element={<MainProfileView />} />
   <Route path='/followvisibilty' element={<MainFollowVisiblity />} /> */}
 
-<Route path="/*" element={<MainFeedNews />} /></>):(<><Route path="/" element={<WellCome />} /><Route path="/signin" element={<SignIn />} />
-  <Route path='/verification' element={<Verification />} />
-  <Route path='/forget' element={<Forget1 />} />
-  <Route path='/user_reset_password/:id/:id/' element={<Forget2 />} /></>)
-}
-
-
-        
+            <Route path="/*" element={<MainFeedNews />} />
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<WellCome />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/verification" element={<Verification />} />
+            <Route path="/forget" element={<Forget1 />} />
+            <Route path="/user_reset_password/:id/:id/" element={<Forget2 />} />
+          </>
+        )}
       </Routes>
       {/* <MainAdminProfile /> Store My Profile Desktop All Component */}
       {/* <MainFeedBack />     Store News Fields All Component */}
